6bd65ff3749833ec496ff2726c801225f018b790
urlana-poojitha <poojiurlana003@gmail.com>
2025-05-26 05:16:00 +0530

    create level 1

---
+++ src/Level1/Task1_TemperatureConverter.java
@@ -0,0 +1,10 @@
+package Level1;
+
+public class Task1_TemperatureConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.err.println("hii");
+	}
+
+}
--- src/Level1/task1.java
+++
@@ -1,8 +0,0 @@
-package Level1;
-
-public class task1 {
-	public static void main(String args[]) {
-		System.out.println("hii");
-	}
-
-}
